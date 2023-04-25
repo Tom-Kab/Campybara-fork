8b27daccad5cec7615e500d97624fee151186f8e
Tom-Kab <marc.aziz02@gmail.com>
2023-04-25 23:47:49 +0300

    Search listView now opens CampsiteDetailsActivity when clicking on an item
    Data is fetched from a local ArrayList of Campsites, should be modified to fetch the data from firebase by providing it with the campsiteId only.

--- app/src/main/java/com/shaygang/campybara/SearchFragment.kt
+++ app/src/main/java/com/shaygang/campybara/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.shaygang.campybara
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -101,6 +102,15 @@ class SearchFragment : Fragment() {
             }
 
         })
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val selectedCampsite = campsiteArrayList[position]
+            val intent = Intent(context, CampsiteDetailsActivity::class.java)
+            intent.putExtra("campsiteName", selectedCampsite.name)
+            intent.putExtra("imageUrl", selectedCampsite.imageUrl)
+            intent.putExtra("ownerUid", selectedCampsite.ownerUID)
+            context?.startActivity(intent)
+        }
     }
 
     private fun campsiteInitialize() {
