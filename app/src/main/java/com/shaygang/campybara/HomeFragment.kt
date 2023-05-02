b8dc87b5e5c9d4b4a8ea023ee6098f674d9ff20d
Tom-Kab <marc.aziz02@gmail.com>
2023-05-02 18:32:03 +0300

    Changed Map of campsite IDs to an array of campsite IDs that fetches the rest of the details from firebase
    Still need to implement that map in the various activities.

--- app/src/main/java/com/shaygang/campybara/HomeFragment.kt
+++ app/src/main/java/com/shaygang/campybara/HomeFragment.kt
@@ -22,6 +22,8 @@ class HomeFragment : Fragment() {
     private lateinit var adapter: CampsiteAdapter
     private lateinit var recyclerView: RecyclerView
     private var campsiteList : ArrayList<Campsite> = arrayListOf()
+    private lateinit var campsiteIdArrayList: ArrayList<String>
+
     private lateinit var campsiteMap : MutableMap<Campsite,String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,22 +49,24 @@ class HomeFragment : Fragment() {
         recyclerView = view.findViewById(R.id.recyclerView)
         recyclerView.layoutManager = layoutManager
         recyclerView.setHasFixedSize(true)
-        adapter = CampsiteAdapter(campsiteMap, campsiteList, requireContext())
+        adapter = CampsiteAdapter(campsiteIdArrayList, campsiteList, requireContext())
         recyclerView.adapter = adapter
     }
 
     private fun campsiteInitialize() {
-        campsiteMap = mutableMapOf()
+        campsiteIdArrayList = arrayListOf()
         databaseRef.addValueEventListener(object : ValueEventListener {
             @SuppressLint("NotifyDataSetChanged")
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // Get all children of myRef
                 for (childSnapshot in dataSnapshot.children) {
+                    val campsiteId = childSnapshot.key!!
                     val imageUrl = childSnapshot.child("imageUrl").value.toString()
                     val campsiteName = childSnapshot.child("name").value.toString()
                     val ownerUid = childSnapshot.child("ownerUID").value.toString()
-                    val locationLat = childSnapshot.child("location").child("0").value as Double
-                    val locationLng = childSnapshot.child("location").child("1").value  as Double
+                    campsiteIdArrayList.add(campsiteId)
+                    val locationLat = childSnapshot.child("location").child("latitude").value as Double
+                    val locationLng = childSnapshot.child("location").child("longitude").value  as Double
                     val location = ArrayList<Double>()
                     location.add(locationLat)
                     location.add(locationLng)
